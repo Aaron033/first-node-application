f05f9f2ee8755a910ac726b8aaedcdec7f2e50b1
Aaron <aaron.botello.h@gmail.com>
2019-03-18 21:11:04 -0500

    shared data

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,2 @@
-const fs = require('fs'); 
-
-//fs.writeFileSync('notes.txt', 'This file was created by Node.js !')
-// document.getElementById("first").addEventListener('input',function(e){
-//     let a = e.target.value; 
-//     console.log(a);
-// });
-console.log("Hi");
-try {
-    fs.appendFileSync('notes.txt',' and this file was created by Aaron Botello using append '); 
-} catch (err){
-    console.log("There was an error");
-}
+const  name = "Aaron"; 
+console.log(name);
