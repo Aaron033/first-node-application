726855847050ca992d7119ecc72bed388e4c1928
Aaron <aaron.botello.h@gmail.com>
2019-03-18 20:58:45 -0500

    console.log  added

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 //fs.writeFileSync('notes.txt', 'This file was created by Node.js !')
 document.getElementById("first").addEventListener('input',function(e){
     let a = e.target.value; 
-    console(a);
+    console.log(a);
 });
 try {
     fs.appendFileSync('notes.txt',' and this file was created by Aaron Botello using append '); 
