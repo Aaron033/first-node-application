faddb3d6cb5e50dab4308b6b3eef7ea3382a2f18
Aaron <aaron.botello.h@gmail.com>
2019-03-20 20:24:38 -0500

    calling a function from other file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,6 @@
-require('./utils.js')
+const add = require('./utils.js')
 
+const sum = add(34, 39)
 // const  name = "Aaron"; 
-console.log(name);
+console.log(sum);
 
--- notes-app/utils.js
+++ notes-app/utils.js
@@ -1,6 +1,10 @@
-console.log("utils.js")
 
-const name = "Samanta";
 
-module.exports 
+
+const add = function (a, b){
+    return a + b;
+
+}
+
+module.exports = add;
 // This how you exports content in this page to another 
