629b202ab5e01e2f7dabd8df20f1f4a60f171fc6
Aaron <aaron.botello.h@gmail.com>
2019-03-24 13:14:44 -0500

    new function created

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,10 @@
 const add = require('./utils.js')
+const rest = require('./utils.js')
 
 const sum = add(34, 39)
 // const  name = "Aaron"; 
 console.log(sum);
 
+const subtract = rest(23 , 4); 
+
+console.log(subtract);
