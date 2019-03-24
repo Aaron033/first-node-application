4a30d65120fca7fa34a7b88dd2446991e5f9143e
Aaron <aaron.botello.h@gmail.com>
2019-03-24 17:36:45 -0500

    npm validator installed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,8 @@
 const add = require('./utils.js');
 const rest = require('./utils.js');
 const getNotes = require('./notes.js')
+const validator = require('validator'); //for npm modules you don't have to provide a path 
+
 // const multy = require('./notes.js');
 
 
-const mynotes = getNotes();
-console.log(mynotes);
-
-// const multyplication = multy(3, 4); 
-// console.log(multyplication, " This is multy");
-
-const sum = add(34, 39)
-// const  name = "Aaron"; 
-console.log(sum, ' This is sum' );
-
-const subtract = rest(23 , 4); 
-
-console.log(subtract, ' This subtract');
