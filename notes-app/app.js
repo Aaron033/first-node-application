f58a9795b79414899c228a40521008eb346ce3bd
Aaron <aaron.botello.h@gmail.com>
2019-03-25 21:03:16 -0500

    validator email

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,6 +2,9 @@
 const getNotes = require('./notes.js')
 const validator = require('validator'); //for npm modules you don't have to provide a path 
 
+const msg = getNotes();
+console.log(msg); 
+console.log(validator.isEmail('aaron.botello.h@gmail.com'))
 // const multy = require('./notes.js');
 
 
