50e541a77499752f6446e2449e782fde8bf26bda
Aaron <aaron.botello.h@gmail.com>
2019-03-25 21:00:44 -0500

    validator created

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,4 @@
-const add = require('./utils.js');
-const rest = require('./utils.js');
+
 const getNotes = require('./notes.js')
 const validator = require('validator'); //for npm modules you don't have to provide a path 
 
