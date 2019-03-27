ccdc347689a50f6462b2eefa258a4512e55f5ad9
Aaron <aaron.botello.h@gmail.com>
2019-03-26 21:21:59 -0500

    chalk installed

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,9 +3,11 @@ const getNotes = require('./notes.js')
 const validator = require('validator'); //for npm modules you don't have to provide a path 
 
 const msg = getNotes();
-console.log(msg); 
-console.log(validator.isEmail('aaron.botello.h@gmail.com'))
-// const multy = require('./notes.js');
+// console.log(msg); 
+// console.log(validator.isEmail('aaron.botello.h@gmail.com'))
+// // const multy = require('./notes.js');
 
-console.log(validator.isURL('http://www.google.com'));
+// console.log(validator.isURL('http://www.google.com'));
+
+
 
