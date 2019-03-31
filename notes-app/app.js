1090acd1244fd9b31557ca0d583ab6dfd5fbbcf9
Aaron <aaron.botello.h@gmail.com>
2019-03-31 16:14:20 -0500

    adding yargs commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,4 +4,16 @@ const yargs = require('yargs')
 const chalk = require('chalk')
 
 // console.log(process.argv); the difference without yargs 
-console.log(yargs.argv);
+
+//Customize yargs version 
+yargs.version('1.1.0') // This is how you change the version 
+// console.log(yargs.argv);
+//It takes an object 
+yargs.command({
+    command: 'add', 
+    describe: 'Add a new note', 
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+
+})
