457736ec055662b5b47f492b911f036af54d8788
Aaron <aaron.botello.h@gmail.com>
2019-03-31 15:55:46 -0500

    node app.js can not be found

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
 
 const getNotes = require('./notes.js')
 const yargs = require('yargs')
+const chalk = require('chalk')
 
-const chalk = require('chalk');
-
-console.log(process.argv);
+// console.log(process.argv); the difference without yargs 
+console.log(yargs.argv);
