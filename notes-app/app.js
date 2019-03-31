3527aa83b2be86014ffff59fed7c2943afe5950c
Aaron <aaron.botello.h@gmail.com>
2019-03-31 15:40:49 -0500

    new file for notes created

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,27 +1,7 @@
 
 const getNotes = require('./notes.js')
-const validator = require('validator'); //for npm modules you don't have to provide a path 
+const yargs = require('yargs')
+
 const chalk = require('chalk');
-const msg = getNotes();
-// console.log(msg); 
-// console.log(validator.isEmail('aaron.botello.h@gmail.com'))
-// // const multy = require('./notes.js');
 
-// console.log(validator.isURL('http://www.google.com'));
-
-console.log(chalk.green('Success'));
-const greenmsg = chalk.green.inverse.bold('Success')
-console.log(greenmsg); 
-
-//The information that we pass after we type node app.js Aaron can be access by process.argv
-console.log(process.argv[2]);
-//process.argv gives everything that you type after:  node app.js *someting 
-//The first 0 and 1 lines are the location of the file and 
-//The third one is whatever you inputted
-const command = process.argv[2]; 
-
-if (command === 'add'){
-    console.log('Adding a note!')
-}else if(command === 'remove'){
-    console.log("Removing note");
-}
+console.log(process.argv);
