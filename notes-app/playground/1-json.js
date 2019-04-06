4532e1c98e3d6a4191ed6b5e941f481b4d507514
Aaron <aaron.botello.h@gmail.com>
2019-04-06 17:10:58 -0500

    converts buffer to string

--- notes-app/playground/1-json.js
+++ notes-app/playground/1-json.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
 
 const dataBuffer = fs.readFileSync('1-json.json') // What comes back from the file is not a string it is a buffer 
-//that is how node.js represents binary data 
+//that is how node.js represents binary data  meaning : 45 74 62 44 
+// In order to changes from data numbers to number you must use toString()
+
+console.log(dataBuffer.toString())
