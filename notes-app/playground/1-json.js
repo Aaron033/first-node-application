0c02a9f983e2af80e438f6bf6c7a8a6b642f9bc2
Aaron <aaron.botello.h@gmail.com>
2019-04-06 22:46:12 -0500

    udemy challenged process steps

--- notes-app/playground/1-json.js
+++ notes-app/playground/1-json.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 
-const dataBuffer = fs.readFileSync('1-json.json') 
+const dataBuffer = fs.readFileSync('1-json.json') //This is required 
 
-const dataJSON = dataBuffer.toString()
-const data = JSON.parse(dataJSON)
+const dataJSON = dataBuffer.toString()//This is required 
+ const data = JSON.parse(dataJSON)//This is required 
+// * They are required because we loaded the file into visual studio code 
 
 
 const myInfo = {
