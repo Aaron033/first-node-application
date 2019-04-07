a54da4ace30ae1cba934dbbff9158db8e7d8d12c
Aaron <aaron.botello.h@gmail.com>
2019-04-06 23:16:34 -0500

    compile issued addressed

--- notes-app/playground/1-json.js
+++ notes-app/playground/1-json.js
@@ -8,9 +8,11 @@ const dataJSON = dataBuffer.toString()//This is required
 
 
 /* This is another way  1.1 */
-user.name = 'Pelos'
-user.age = 21 
+data.name = 'Pelos'
+data.age = 21 
+// It is assumed tha the user will take the valued that 
+// --> we assgined manually (check user.name )
 
-const myinfo = JSON.stringify(user)
-fs.writeFileSync('1-json.json', myInfo)
+const myinfo = JSON.stringify(data) // It converts  the object into string and changes the value 
+console.log(myinfo)
 /* This is another way  1.1 */
