e5142011de8b1678a6d2d50cae2ebc61002fffe7
Aaron <aaron.botello.h@gmail.com>
2019-04-06 21:05:47 -0500

    object changed

--- notes-app/playground/1-json.js
+++ notes-app/playground/1-json.js
@@ -1,10 +1,7 @@
 const fs = require('fs')
 
-const dataBuffer = fs.readFileSync('1-json.json') // What comes back from the file is not a string it is a buffer 
-//that is how node.js represents binary data  meaning : 45 74 62 44 
-// In order to changes from data numbers to number you must use toString()
-
+const dataBuffer = fs.readFileSync('1-json.json') 
 const dataJSON = dataBuffer.toString()
 const data = JSON.parse(dataJSON)
 
-console.log(data)
+console.log(data.title)
--- notes-app/playground/extra-notes.js
+++ notes-app/playground/extra-notes.js
@@ -2,8 +2,8 @@ const car ={
     make: "volkswagen", 
     model: "cc"
 }
-const bookJSON = JSON.stringify(book)
-console.log(bookJSON) // this converts the objet to a string 
+const bookJSON = JSON.stringify(book)// this converts the objet to a string 
+console.log(bookJSON) 
 //const book = {
     //  title: 'Ego is the Enemy', 
     //  author: 'Aaron Botello'
@@ -12,7 +12,9 @@ console.log(bookJSON) // this converts the objet to a string
     // const bookJSON = JSON.stringify(book)
     // fs.writeFileSync('1-json.json', bookJSON)
     
-
+    const dataBuffer = fs.readFileSync('1-json.json') // What comes back from the file is not a string it is a buffer 
+    //that is how node.js represents binary data  meaning : 45 74 62 44 
+    // In order to changes from data numbers to number you must use toString()
 
 const parseData = JSON.parse(bookJSON)//This code converts the string into an object
-console.log(parseData.author); 
+console.log(parseData); 
