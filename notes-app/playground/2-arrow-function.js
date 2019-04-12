cba1238bcfb195c20c1c36b13215846823160856
Aaron <aaron.botello.h@gmail.com>
2019-04-11 23:06:58 -0500

    Es6

--- notes-app/playground/2-arrow-function.js
+++ notes-app/playground/2-arrow-function.js
@@ -1,5 +1,10 @@
-const square =function(x){
-return x * x 
+// const square =function(x){
+// return x * x 
+// }
+
+//Same functionality 
+const square = (x) => {
+    return x * x 
 }
 
 console.log(square(3))
