8e65d699f603b79f21b2d18b0abda69cf1982b06
Aaron <aaron.botello.h@gmail.com>
2019-04-12 21:37:08 -0500

    Es6 function definition sytac used

--- notes-app/playground/2-arrow-function.js
+++ notes-app/playground/2-arrow-function.js
@@ -20,7 +20,7 @@ const event = {
  printGuestList: function(){
      console.log('Guest list for ' + this.name) // this. has the ability to acces 
      // properties of objects
-     this.guestList.forEach(function(guest){// with (guest) you access every item in gueslist
+     this.guestList.forEach((guest)=> {// with (guest) you access every item in gueslist
 console.log(guest + ' is attending' + this.name)
      })
  }
