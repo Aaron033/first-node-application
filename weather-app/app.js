76701b9c14b2cb2e041868a625355d9a46b46ae1
Aaron <aaron.botello.h@gmail.com>
2019-04-15 19:53:14 -0500

    Explaining the content of asynchronous functions

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,6 @@
+//In an asynchronous functions the statements runs from top to bottom 
+//So staring and stopping will print out and at the end the 2 second timer will print out 
+
 console.log('Starting')
 
 setTimeout(()=> {
