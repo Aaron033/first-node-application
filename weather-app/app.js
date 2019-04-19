1877e7740eb3637a79c908197946a0af2daa31d1
Aaron <aaron.botello.h@gmail.com>
2019-04-19 17:08:43 -0500

    usiing mapbox.com services

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,6 +6,7 @@ request({url: url, json:true }, (error, response) => { // The json: option is go
     // jSON.parse(response.body) option 
 // console.log(response.body.currently)
 // console.log(response.body.timezone)
+//in foward geocoding we type the string address and we get the coordinates 
 console.log(response.body.daily.data[0].summary +" It's currenctly " + response.body.currently.temperature + " degrees out. There is a "+ response.body.currently.precipProbability +" chance of rain."  )
 console.log(response.body.daily.data[0].moonPhase)
 console.log(response.body.daily.data[1].moonPhase)
