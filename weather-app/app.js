cb2e30694104be8e78904a72bc51b3832210122f
Aaron <aaron.botello.h@gmail.com>
2019-04-19 12:47:32 -0500

    updates info

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,13 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/xb95952/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/4x228a7b95952/37.8267,-122.4233'
 
 request({url: url, json:true }, (error, response) => { // The json: option is going to parse the data so there is not need of 
     // jSON.parse(response.body) option 
 // console.log(response.body.currently)
 // console.log(response.body.timezone)
-console.log("It's currenctly " + response.body.currently.temperature + " degrees out. There is a "+ response.body.currently.precipProbability +" chance of rain."  )
+console.log(response.body.daily.data[0].summary +" It's currenctly " + response.body.currently.temperature + " degrees out. There is a "+ response.body.currently.precipProbability +" chance of rain."  )
+console.log(response.body.daily.data[0].moonPhase)
+console.log(response.body.daily.data[1].moonPhase)
+console.log(response.body.daily.data[2].moonPhase)
 })
