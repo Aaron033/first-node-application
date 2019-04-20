1ad78323a6575430576cd8dba479551a65c99b33
Aaron <aaron.botello.h@gmail.com>
2019-04-20 09:56:17 -0500

    4-callback.js file added

---
+++ notes-app/playground/4-callbacks.js
@@ -0,0 +1,6 @@
+//A callback function is a function that we pass as an argument to another function with the intention of 
+// Call it later on 
+
+setTimeout(() =>{
+    console.log('Two seconds are up')
+}, 2000)
