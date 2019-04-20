c475a11d6785d1d470b304c432653bc0adf64569
Aaron <aaron.botello.h@gmail.com>
2019-04-20 10:55:29 -0500

    Callback challenged accepted

--- notes-app/playground/4-callbacks.js
+++ notes-app/playground/4-callbacks.js
@@ -22,5 +22,17 @@ const geocode = (address, callback) => {
 }
 
 geocode('Houston', (data) => { //The data in passed as an argument can be anything (named)
+console.log(data)
+})
 
+// This is the challenge 
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
 })
