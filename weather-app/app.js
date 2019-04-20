020a1464c17e958d098a24c009a4c844b7d14e51
Aaron <aaron.botello.h@gmail.com>
2019-04-19 22:40:33 -0500

    mapbox location issue fixed

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/
+const url = 'https://api.darks
 //The error function is a low level information for example (not connectivity)
 request({url: url, json:true }, (error, response) => { // The json: option is going to parse the data so there is not need of 
 //in foward geocoding we type the string address and we get the coordinates 
@@ -17,11 +17,16 @@ console.log('Unable to connect to weather')
 }
 })
 
-const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_t
+const url1 = 'https://api.mapbox..gPhF3jbAGPQxXgJzZfdoRg&limit=1'
 request({url: url1, json: true}, (error, response) =>{ //The url must stay the same 
     if(error){
 console.log('Unable to connect to Weather Serives!')
-    }else{
+    } else if(response.body.features[0] === undefined){
+        
+
+        console.log('Not such location')
+    }
+    else{
         const latitude = response.body.features[0].center[1]
         const longitude = response.body.features[0].center[0]
         console.log('The latitud is: '+ latitude + '\n'+ 'The longitud is: '+longitude )
