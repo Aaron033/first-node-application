4f70336347c700f7f7a8b1c58890e4f69f32fe61
Aaron <aaron.botello.h@gmail.com>
2019-04-21 17:08:06 -0500

    process.argv functionality added  to geocode function

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,19 +3,11 @@ const request = require('request');
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-
-
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+//console.log(process.argv) is a predefined function 
+const address = process.argv[2] 
 
     //We pass Houston as a parameter 
-geocode('Houston', (error, data) =>{
+geocode(address, (error, data) =>{
 //return is going to stop the function execution after the error message its print out 
 if(error) {
     return console.log(error)
