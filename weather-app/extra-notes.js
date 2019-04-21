26c7842b1a91858e2e98b84e45089aa60e44c959
Aaron <aaron.botello.h@gmail.com>
2019-04-20 21:31:14 -0500

    geocode file added

--- weather-app/extra-notes.js
+++ weather-app/extra-notes.js
@@ -30,3 +30,32 @@ request({url: url, json:true }, (error, response) => { // The json: option is go
 console.log(response.body.daily.data[0].summary +" It's currenctly " + response.body.currently.temperature + " degrees out. There is a "+ response.body.currently.precipProbability +" chance of rain."  )
 
 })
+
+const url = 'https://api.darksky.net/forecast/42
+//The error function is a low level information for example (not connectivity)
+
+request({url: url, json:true }, (error, response) => { // The json: option is going to parse the data so there is not need of 
+//in foward geocoding we type the string address and we get the coordinates 
+if(error){
+console.log('Unable to connect to weather')
+}else if(response.body.error){
+    console.log('Unable to find location')
+}else{
+    console.log(response.body.daily.data[0].summary +" It's currenctly " + response.body.currently.temperature + " degrees out. There is a "+ response.body.currently.precipProbability +" chance of rain."  )
+}
+})
+
+const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/12what.json?access_token=
+request({url: url1, json: true}, (error, response) =>{ //The url must stay the same 
+    if(error){
+console.log('Unable to connect to Weather Serives!')
+    } else if(response.body.features[0] === undefined){
+        console.log('Not such location')
+    }
+    else{
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log('The latitud is: '+ latitude + '\n'+ 'The longitud is: '+longitude )
+    }
+    
+})
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    //encodeURIComponent  is for special characters 
+         const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.'
+         request({url: url, json: true}, (error, response)=>{
+            if(error){
+                //If this code is trigged; The function could have connectivity issues 
+                //We are calling the callback function 
+                callback('Unable to connect to services', undefined)
+            }else if (response.body.features.length === 0){
+              callback('Unable to find location', undefined)
+            }else{
+                //We set a undefined parameter because, previous errors had been take cared off 
+             callback(undefined, {
+                latitude: response.body.features[0].center[0], 
+                longitud: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+                
+             }) 
+            }
+         })
+        }
+        
+
+        module.exports = geocode
