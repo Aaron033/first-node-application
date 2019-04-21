e9a4e74a99784f77b1cf5c485613195cb9634cd2
Aaron <aaron.botello.h@gmail.com>
2019-04-20 23:03:51 -0500

    forecast function not working :

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,5 +1,18 @@
 const request = require('request')
 
-const forecast = () =>{
-    const url = 
+const forecast = (latitude, longitude, callback) =>{
+   
+    const url = 'https://api.darksky.net/forecast//' + latitude + ',' + longitude 
+    request({url: url, json: true}, (error, response)=>{
+if(error){
+    callback('Unable to connect to a server', undefined)
+}else if(response.body.error){
+    callback("Address not found", undefined)
+}else{
+    callback(undefined, response.body.daily.data[0].summmary)
+   
 }
+    })
+}
+
+module.exports = forecast
