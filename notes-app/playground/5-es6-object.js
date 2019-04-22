8610d39c10c64bc227b24cd57374aa224ace7924
Aaron <aaron.botello.h@gmail.com>
2019-04-21 21:38:37 -0500

    working with  Object distructuing

--- notes-app/playground/5-es6-object.js
+++ notes-app/playground/5-es6-object.js
@@ -9,5 +9,20 @@ const user = {
     location: 'Houston'
 }
 
+
+// const user = {
+//     name,   // The name has to match up with the variable defined 
+//     userAge, 
+//     location: 'Houston'
+// }
+
 console.log(user)
 console.log(user.age)
+
+//Object desctructuring 
+const product = {
+    label: 'Red notebook', 
+    price: 3, 
+    stock: 201, 
+    salePrice: undefined
+}
