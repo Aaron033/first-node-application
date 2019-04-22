5f602888137bacf85ff906b20797823f3d3aaa2d
Aaron <aaron.botello.h@gmail.com>
2019-04-21 21:31:41 -0500

    new file in the play ground added

---
+++ notes-app/playground/5-es6-object.js
@@ -0,0 +1,13 @@
+//Obejct property shorthand 
+
+const name = "Aaron"
+const userAge = 25
+
+const user = {
+    name: name, 
+    age: userAge, 
+    location: 'Houston'
+}
+
+console.log(user)
+console.log(user.age)
