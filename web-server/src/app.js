7da6adf9f8a2c8c306eb103c8a16fe43bdb89726
Aaron <aaron.botello.h@gmail.com>
2019-04-23 21:04:02 -0500

    using res.send to feedback

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,3 +1,11 @@
 const express = require('express')
 
 const app = express() //This is how we are going to access express properties 
+// app.com  = Domain , route
+// app.com/help 
+// app.com/about 
+//The function in the second argument is what we want to do when people visits this page
+app.get('', (req, res) =>{
+    //res.send() send something back to the requester 
+   res.send()
+})
