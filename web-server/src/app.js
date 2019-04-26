eef07f5e143af41698c365fff366af39ba1280c9
Aaron <aaron.botello.h@gmail.com>
2019-04-25 23:05:34 -0500

    static file created

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,15 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-//This is how we locate a file in the system 
-console.log(path.join(__dirname, '../public'))
+// console.log(__dirname)
+// //This is how we locate a file in the system 
+// console.log(path.join(__dirname, '../public'))
 const app = express() //This is how we are going to access express properties 
 // app.com  = Domain , route
 // app.com/help 
 // app.com/about 
+const publicDirectoryPath = path.join(__dirname, '../public') 
+app.use(express.static(publicDirectoryPath))
 //The function in the second argument is what we want to do when people visits this page
 app.get('', (req, res) =>{
     //res.send() send something back to the requester 
