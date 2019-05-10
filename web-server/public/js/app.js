041e8814aa0412c12bef73c025daa0d13c3753c3
Aaron <aaron.botello.h@gmail.com>
2019-05-10 14:28:36 -0500

    setting up a search box for location

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,15 +1,22 @@
 console.log('Client side javascript file is loaded')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) =>{ 
-    response.json().then((data) => {
-    //response.json is a function the second.then is going to run when the json data has arrived and then parse
-    console.log(data)
-    })
+// fetch('http://puzzle.mead.io/puzzle').then((response) =>{ 
+//     response.json().then((data) => {
+//     //response.json is a function the second.then is going to run when the json data has arrived and then parse
+//     console.log(data)
+//     })
 
-})
+// })
+
 
 fetch('http://localhost:3000/weather?address=Boston').then((response) => {
     response.json().then((data) =>{
-        console.log(data)
+       if(data.error){
+           console.log(data.error)
+       }else{
+        console.log(data.location)
+        console.log(data.forecast)
+       
+       }
     })
 })
