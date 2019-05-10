293d9d78a84397fc411edfafc61f1904eefa37b5
Aaron <aaron.botello.h@gmail.com>
2019-05-10 13:41:22 -0500

    app.js in the public folder bugs fixed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,3 +1,15 @@
-console.log('Client side javascript file is loaded');
+console.log('Client side javascript file is loaded')
 
-fetch('http://puzzle.mead.io/puzzle')
+fetch('http://puzzle.mead.io/puzzle').then((response) =>{ 
+    response.json().then((data) => {
+    //response.json is a function the second.then is going to run when the json data has arrived and then parse
+    console.log(data)
+    })
+
+})
+
+fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+    response.json().then((data) =>{
+        console.log(data)
+    })
+})
--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     //encodeURIComponent  is for special characters 
-         const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'
+         const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token='
          request({url, json: true}, (error, {body})=>{
             if(error){
                 //If this code is trigged; The function could have connectivity issues 
