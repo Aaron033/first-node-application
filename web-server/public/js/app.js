05b9c8853b74df6d9ba4deecba6d9f3b462c40cd
Aaron <aaron.botello.h@gmail.com>
2019-05-09 20:21:27 -0500

    random puzzle query 1.2

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,3 +1,5 @@
 console.log('Client side javascript file is loaded');
 
 fetch('http://puzzle.mead.io/puzzle')
+console.log('Client side javascript file is loaded');
+console.log('Client side javascript file is loaded');
