258e834eab40c90fde3a57f6a035ec00ccabd337
Aaron <aaron.botello.h@gmail.com>
2019-05-11 08:12:13 -0500

    address provided to app js using the form input value

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -8,17 +8,6 @@ console.log('Client side javascript file is loaded')
 
 // })
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-    response.json().then((data) =>{
-       if(data.error){
-           console.log(data.error)
-       }else{
-        console.log(data.location)
-        console.log(data.forecast)
-       
-       }
-    })
-})
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -29,5 +18,25 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value 
     //The value is the information that we typed in the search box
-    console.log('testing!')
+
+if(!location) {
+    console.log('Must provide location')
+}else {
+    const show = fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) =>{
+           if(data.error){
+               console.log(data.error)
+           }else{
+            console.log(data.location)
+            console.log(data.forecast)
+           
+           }
+        })
+    })
+    
+    console.log(show)
+
+}
+
+
 })
